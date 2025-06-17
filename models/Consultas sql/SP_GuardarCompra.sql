0dc54fc5051fbb4ceb11491ac2d1f5c42da1c0be
mercedesduarte <mercedesduarte472006@gmail.com>
2025-06-17 16:11:59 -0300

    guardar compra

--- models/Consultas sql/SP_GuardarCompra.sql
+++ models/Consultas sql/SP_GuardarCompra.sql
@@ -3,7 +3,10 @@ GO
 CREATE PROCEDURE SP_GuardarCompra
     @id_usuario INT,
     @id_viaje INT,
-    @cantidad INT
+    @cantidad INT,
+    @transporte nvarchar(30),
+    @estadia nvarchar(30)
+AS
 AS
 BEGIN
     INSERT INTO Compras (Id_Viaje, Id_Usuario, Num_Compra, Estado_Compra, Fecha_Compra, Cant_Compra)
---
+++ models/models/M_Comprar.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/conexionbd.php';
+
+class SP_ComprarPro{
+    private $conn;
+
+    public function __construct(){
+        $this-> conn = Conexion::ConexionBD();
+    }
+
+    public function comprar($usuario, $fechasalida, $fechavuelta, $cantidad, $transporte, $estadia){
+
+        $sqle -> query("SELECT Id_Viaje FROM Viajes WHERE Fecha_Salida = $fechasalida AND Fecha_Vuelta = $fechavuelta")
+        $id_viaje = $sqle -> execute()
+        $sql = "EXEC SP_GuardarCompra ?, ?, ?, ?, ?";
+        $stmt = $this -> conn -> prepare($sql)
+        $stmt = execute([$usuario, $id_viaje, $cantidad, $transporte, $estadia]);
+        return $stmt;
+    }
+}
+
+?>
